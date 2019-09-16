e1dc8c4d4f70f7365b8f771cf05b852c7485ddfa
hhugohm <hhugohm@gmail.com>
2019-09-16 12:05:34 -0500

    first commit

---
+++ app.groovy
@@ -0,0 +1,12 @@
+
+@Controller
+@Grab('spring-boot-starter-freemarker')
+class MyApp {
+
+    @RequestMapping("/greet")
+        String home(Model model, @RequestParam String name) {
+		
+		model.addAttribute("myname", name)
+		return "hello"
+	}
+ }
